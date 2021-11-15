352ba9bd282df36b778d5122a0e3adfae5a58d70
Ghidyon <gideonakunana@gmail.com>
2021-11-15 14:16:13 +0100

    ci: added IdentityContext

---
+++ WalletApi.Models/Entities/IdentityContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletApi.Models.Entities
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(entity => entity.ToTable(name: "Users"));
+
+            builder.Entity<IdentityRole>(entity => entity.ToTable(name: "Roles"));
+
+            builder.Entity<IdentityUserRole<string>>(entity => entity.ToTable(name: "UserRoles"));
+
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.ToTable(name: "UserClaims"));
+
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.ToTable(name: "UserLogins"));
+
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.ToTable(name: "RoleClaims"));
+
+            builder.Entity<IdentityUserToken<string>>(entity => entity.ToTable(name: "UserTokens"));
+
+            builder.Entity<User>().HasQueryFilter(u => !u.IsDeleted || !u.IsDeactivated);
+
+            builder.Entity<Wallet>().HasQueryFilter(w => !w.IsDeleted || !w.IsDeactivated);
+
+            builder.Entity<Transaction>().HasQueryFilter(t => !t.IsDeleted || !t.IsDeactivated);
+
+        }
+
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
--- WalletApi.Models/Entities/User.cs
+++ WalletApi.Models/Entities/User.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WalletApi.Models.Interfaces;
 
 namespace WalletApi.Models.Entities
