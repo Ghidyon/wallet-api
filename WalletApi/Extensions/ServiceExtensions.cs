0e4ffb887f77f0efa89e63dad386e1f85cf941f2
Ghidyon <gideonakunana@gmail.com>
2021-11-15 14:55:25 +0100

    feat: added logger service

--- WalletApi/Extensions/ServiceExtensions.cs
+++ WalletApi/Extensions/ServiceExtensions.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WalletApi.Models.Entities;
 
 namespace WalletApi.Extensions
 {
@@ -16,5 +19,33 @@ namespace WalletApi.Extensions
                 .AllowAnyMethod()
                 .AllowAnyHeader());
             });
+
+        public static IServiceCollection ConfigureDbContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+                services.AddDbContext<IdentityContext>(options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("WalletConnection"),
+                        b => b.MigrationsAssembly("WalletApi"));
+                });
+        }
+
+        public static IServiceCollection ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 10;
+                options.User.RequireUniqueEmail = true;
+            });
+
+            return services;
+        }
     }
 }
